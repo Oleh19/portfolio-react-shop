e3a945bcf255c345128ba8ad65ea4ba2410764bf
Oleh19 <105211686+Oleh19@users.noreply.github.com>
2022-12-24 19:53:59 +0200

    Updates

--- static/js/949.1dd7fc46.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkportfolio_react_artworks=self.webpackChunkportfolio_react_artworks||[]).push([[949],{4949:function(r,t,o){o.r(t);o(2791);var e=o(184);t.default=function r(){return(0,e.jsx)(r,{})}}}]);
-//# sourceMappingURL=949.1dd7fc46.chunk.js.map
